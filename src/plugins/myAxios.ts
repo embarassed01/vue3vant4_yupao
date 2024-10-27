a4136da9f55ae6590250dbc070991da36c98daf0
embarassed01 <13854210651@163.com>
2024-10-27 09:44:20 +0800

    done+上线dev

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// 需要安装 @types/node 否则不认识process
+//  npm i --save-dev @types/node
+const isDev = process.env.NODE_ENV === 'development';
+// 这种方式，需要后端Java项目开启跨域支持！（目前后端已经支持了 WebMvcConfig.java
 const myAxios = axios.create({
     // baseURL: 'http://localhost:8080/api'
+    // baseURL: isDev ? 'http://localhost:8080/api' : 'https://yupao-backend-974538-1256524210.ap-shanghai-run.tcloudbase.com'   //'线上接口'
 });
 myAxios.defaults.withCredentials = true;  // 表示在向后端发送请求时默认携带上cookie
 
